5357dbee61bf24b5ce858432ac740892f85ffcfd
Khinshan Khan <khanshan501@gmail.com>
2021-04-13 14:42:40 -0400

    called api and handled error cases

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const City = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState(null);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState(null);
 
+  const fetchCities = async () => {
+    axios
+      .get(`http://ctp-zip-api.herokuapp.com/city/SPRINGFIEL`)
+      .then(res => {
+        setMessage(null);
+        setResults(res.data);
+      })
+      .catch(err => {
+        setResults(null);
+        setMessage({
+          status: err.response.status,
+          text: err.response.statusText
+        });
+      });
+  };
+
+  console.log(results);
   return (
     <>
       <h1>City Search</h1>
       {/* TODO: setup input */}
 
+      <button onClick={fetchCities}>Search</button>
+      <br />
       {/* TODO: pretty format loading */}
-      {message === "" && results == null && <>Loading!</>}
+      {message == null && results == null && <>Loading!</>}
       {/* TODO: pretty format error message */}
-      {message !== "" && results == null && (
-        <>Something went wrong: {message}</>
+      {message != null && results == null && (
+        <>
+          Something went wrong! <br />
+          {message.status}: {message.text}
+        </>
       )}
-      {results != null && results.map(result => <p>"hello"</p>)}
+
+      {results != null && "hello"}
     </>
   );
 };

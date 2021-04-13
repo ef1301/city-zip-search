08e3746a7200c39c8a5c33c7686ae368e2abdb5d
Khinshan Khan <khanshan501@gmail.com>
2021-04-13 14:26:30 -0400

    figured out city components

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const City = () => {
-  return <>city</>;
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState(null);
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <h1>City Search</h1>
+      {/* TODO: setup input */}
+
+      {/* TODO: pretty format loading */}
+      {message === "" && results == null && <>Loading!</>}
+      {/* TODO: pretty format error message */}
+      {message !== "" && results == null && (
+        <>Something went wrong: {message}</>
+      )}
+      {results != null && results.map(result => <p>"hello"</p>)}
+    </>
+  );
 };
 
 export default City;
